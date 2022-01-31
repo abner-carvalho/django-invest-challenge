a6e20f8caa6f104e26c30aee967776d8c0f1ad4b
Abner Carvalho Almeida <abnercarvalhoalmeida@gmail.com>
2022-01-31 15:55:26 -0300

    :construction: Created the Asset Model and regirstered in the admin

--- djangoinvest/assets/models.py
+++ djangoinvest/assets/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Asset(models.Model):
+    long_name  = models.CharField(max_length=200)
+    symbol     = models.CharField(max_length=50)
+    thumbnail  = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    # Metadados
+    class Meta:
+        ordering = ['-symbol']
+
+    def publish(self):
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.symbol
